f88228697a9f98368a1b999d7fc73aa61bf4444b
Marcelo Vaquilema <luis_vaquilema@hotmail.com>
2024-08-14 11:28:04 -0500

    corrigiendo gitignore

--- src/Index.tsx
+++ src/Index.tsx
@@ -1,7 +1,7 @@
-import DynamicTable from './components/Index'
+import MobileTable from './components/Index'
 import './main.css'
 import './css/other.css'
 import './css/modal.css'
 
 export * from './types'
-export default DynamicTable
+export default MobileTable
